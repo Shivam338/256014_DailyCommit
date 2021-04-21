b2b1420e96d4aefb02234c4d64e6507d49a00968
Shivam Dogra <dogra156@gmail.com>
2021-04-22 04:45:39 +0530

    Math Library Examples.

---
+++ Math_Module.py
@@ -0,0 +1,47 @@
+from math import sin, pi
+
+print(sin(pi / 2))
+
+pi = 3.14
+
+
+def sin(x):
+    if 2 * x == pi:
+        return 0.99999999
+    else:
+        return None
+
+
+print(sin(pi / 2))
+
+# Output = 1.0, 0.99999999
+
+# ---------------------- 2nd Program ---------------------- #
+from math import pi, radians, degrees, sin, cos, tan, asin
+
+ad = 90
+ar = radians(ad)
+ad = degrees(ar)
+
+print(ad == 90.)
+print(ar == pi / 2.)
+print(sin(ar) / cos(ar) == tan(ar))
+print(asin(sin(ar)) == ar)
+
+# Output = All returned as TRUE
+
+# ---------------------- 3rd Program ---------------------- #
+from math import ceil, floor, trunc
+
+x = 1.4
+y = 2.6
+
+print(floor(x), floor(y))
+print(floor(-x), floor(-y))
+print(ceil(x), ceil(y))
+print(ceil(-x), ceil(-y))
+print(trunc(x), trunc(y))
+print(trunc(-x), trunc(-y))
+
+# Output= 1 2, -2 -3, 2 3, -1 -2, 1 2, -1 -2
+
